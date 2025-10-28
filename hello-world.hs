93a6250b758d30e7d01d7af7ca4e0e70f6f884d6
sshduck <d00479329@utahtech.edu>
2025-10-28 16:57:33 -0600

    hello world

---
+++ hello-world.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = putStrLn "Hello, world!"
+-- this is a comment
+{- Multiline comment-} 
+
+{-
+main func is haskell's entry point in each program
+putStrLn is part of the default std lib called Prelude
+it prints a string to the console followed by a newline
+-}
